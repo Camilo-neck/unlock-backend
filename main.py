6c0d9d78284f58bc8117309d679c22b1facf49b8
jorozcove <jorozcove@unal.edu.co>
2024-08-18 19:35:35 -0500

    removed users tests

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import uvicorn
 app = FastAPI()
 
 # Import and register the API endpoints
-from src.routes.users import users_router
 from src.routes.events import events_router
 
 from src.supa.client import sb
@@ -13,7 +12,7 @@ from src.middlewares.auth import CustomAuthMiddleware
 # Add middleware
 app.add_middleware(CustomAuthMiddleware, supabase=sb)
 
-app.include_router(users_router, prefix="/users")
+# Add the routers
 app.include_router(events_router, prefix="/events")
 
 # Run the FastAPI app
--- src/controllers/users.py
+++
@@ -1,7 +0,0 @@
-from typing import List
-from src.supa.client import sb
-
-from src.models.users import User
-
-def get_all_users() -> List[User]:
-    return
