323f1e8d53de5eb218fc9deae4b65bd2c0ec1358
jorozcove <jorozcove@unal.edu.co>
2024-08-25 17:12:05 -0500

    user service role key instead of public

--- src/supa/client.py
+++ src/supa/client.py
@@ -2,5 +2,6 @@ from supabase import create_client, Client
 from src.core.config import settings
 
 url: str = settings.supabase_url
-key: str = settings.supabase_key
+# key: str = settings.supabase_key
+key: str = settings.supabase_service_role_key
 sb: Client = create_client(url, key)
