846794205997522c18e8e5c36754c55cb80993db
jorozcove <jorozcove@unal.edu.co>
2024-08-25 15:46:06 -0500

    get by id

--- src/routes/events.py
+++ src/routes/events.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Depends, Request
 from fastapi.security import HTTPAuthorizationCredentials
 
 from typing import List
+import uuid
 from src.models.events import Event
 from src.schemas.events import EventCreate
 import src.controllers.events as events
@@ -18,7 +19,14 @@ async def create_event(request: Request, event_create: EventCreate, token: HTTPA
     return await events.create_event(event_create, admin_id = request.state.user.id)
 
 # Get all events of the admin-user
-@events_router.get("/")
+@events_router.get("/admin")
 @protected_route()
 async def get_admin_events(request: Request, token: HTTPAuthorizationCredentials = Depends(auth_scheme)) -> List[Event]:
     return await events.get_admin_events(admin_id = request.state.user.id)
+
+# Get event by id
+@events_router.get("/{event_id}")
+@protected_route()
+async def get_event_by_id(event_id: uuid.UUID, request: Request, token: HTTPAuthorizationCredentials = Depends(auth_scheme)) -> Event:
+    return await events.get_event_by_id(event_id, admin_id = request.state.user.id)
+
